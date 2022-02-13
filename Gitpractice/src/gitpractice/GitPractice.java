b90b6891290f99a474fa6781cb3181fe31f4611d
jasonvalladolid <Jason>
2022-02-13 11:59:41 -0500

    my first commit

---
+++ Gitpractice/src/gitpractice/GitPractice.java
@@ -0,0 +1,11 @@
+package gitpractice;
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello my name is Jason jbwdjkcbwkjbwe");
+	}
+
+}
