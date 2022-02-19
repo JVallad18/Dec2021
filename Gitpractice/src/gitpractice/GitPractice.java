8d7abe3a33f1ec6a1358d13d019461ac715221aa
jasonvalladolid <Jason>
2022-02-19 13:56:27 -0500

    new changes

--- Gitpractice/src/gitpractice/GitPractice.java
+++ Gitpractice/src/gitpractice/GitPractice.java
@@ -6,6 +6,7 @@ public class GitPractice {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello my name is Jason jbwdjkcbwkjbwe");
+		System.out.println("Hello my name is Jason jbwdjkcbwkjbwe");
 	}
 
 }
