179e38e2711d18a1173c2a53119232802d208524
jasonvalladolid <Jason>
2022-02-19 14:08:29 -0500

    new commit

--- Gitpractice/src/gitpractice/GitPractice.java
+++ Gitpractice/src/gitpractice/GitPractice.java
@@ -4,9 +4,9 @@ public class GitPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello my name is Jason jbwdjkcbwkjbwenfe");
 		System.out.println("Hello my name is Jason jbwdjkcbwkjbwe");
-		System.out.println("Hello my name is Jason jbwdjkcbwkjbwe");
+		System.out.println("Hello my name is Jason jbwdjkcbwkjbwenfe");
 	}
 
 }
